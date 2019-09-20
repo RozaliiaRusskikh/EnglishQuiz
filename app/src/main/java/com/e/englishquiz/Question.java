2f323d16f387f55baf559667589040448111d401
rrrusskikh <rrrusskikh@gmail.com>
2019-09-21 01:34:31 +0300

    Next button added

--- app/src/main/java/com/e/englishquiz/Question.java
+++ app/src/main/java/com/e/englishquiz/Question.java
@@ -1,4 +1,27 @@
 package com.e.englishquiz;
 
 public class Question {
+    private int mQuestionTextId;
+    private boolean mIsAnswerTrue;
+
+    public Question(int questionTextId, boolean answerTrue) {
+        mQuestionTextId = questionTextId;
+        mIsAnswerTrue = answerTrue;
+    }
+
+    public int getQuestionTextId() {
+        return mQuestionTextId;
+    }
+
+    public void setQuestionTextId(int questionTextId) {
+        mQuestionTextId = questionTextId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mIsAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mIsAnswerTrue = answerTrue;
+    }
 }
