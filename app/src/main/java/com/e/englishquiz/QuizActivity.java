0b3ffa991bb8a0b123a5789f40ba2e96a4889a56
rrrusskikh <rrrusskikh@gmail.com>
2019-09-21 18:14:32 +0300

    Several questions have been added

--- app/src/main/java/com/e/englishquiz/QuizActivity.java
+++ app/src/main/java/com/e/englishquiz/QuizActivity.java
@@ -6,12 +6,28 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView mQuestionTextView;
+    private Button mNextButton;
+    private Question Questions[];
+
+    private Question [] mquestions = new Question[]{
+            new Question(R.string.question_about_auxiliary_verb, false),
+            new Question(R.string.question_about_must, false),
+            new Question(R.string.question_about_phrasal_verb, false),
+            new Question(R.string.question_about_shortest_sentence, false),
+            new Question(R.string.question_about_used_to_do, false),
+            new Question(R.string.question_about_verb_to_think, true),
+            new Question(R.string.question_about_verb_to_do, false)
+    };
+
+    private  int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
