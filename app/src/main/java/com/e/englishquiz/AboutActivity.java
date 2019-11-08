0fa46f974c309e57948f6f088a3044ee90538f85
rrrusskikh <rrrusskikh@gmail.com>
2019-11-08 11:17:56 +0300

    Add toolbar

--- app/src/main/java/com/e/englishquiz/AboutActivity.java
+++ app/src/main/java/com/e/englishquiz/AboutActivity.java
@@ -3,12 +3,16 @@ package com.e.englishquiz;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
