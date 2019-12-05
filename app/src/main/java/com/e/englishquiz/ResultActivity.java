1ed85b19d6bcbb4079ef6d1fe20dc2c8eba9c385
Rose <rrrusskikh@gmail.com>
2019-12-05 16:41:08 +0300

    New activity and layout have been added

---
+++ app/src/main/java/com/e/englishquiz/ResultActivity.java
@@ -0,0 +1,27 @@
+package com.e.englishquiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
+
